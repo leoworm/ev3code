9d081036b85ae7b7924b8abf3fee49f891aeb7a4
leoworm <leo.charles.p@gmail.com>
2017-04-25 21:07:53 +0200

    Should now work with SSH

--- laptop.js
+++ laptop.js
@@ -1,8 +1,31 @@
+//TODO switch to http and sockets so electron can be used to make a GUI
+//All modules are required here. They must be installed with the --save argument
 var colors = require('colors');
 var keypress = require('keypress');
-var open = require('open');
+var ev3 = require('ev3dev-lang');
 
-open('http://google.com');
+var leftMotor = new ev3.LargeMotor('port')
+var rightMotor = new ev3.LargeMotor('port')
+
+var leftConnected = true;
+var rightConnected = true;
+
+//Checking if the motors are connected
+if(leftMotor.connected != true) {
+  var leftConnected = false
+  console.log("The left motor doesn't seem to be connected.".yellow);
+}
+if(rightMotor.connected != true) {
+  var rightConnected = false
+  console.log("The right motor doesn't seem to be connected.".yellow);
+}
+if(rightConnected==false || leftConnected==false) {
+  console.log('Something is wrong, aborting...'.red);
+  process.exit();
+}else{
+  console.log("Motors seem connected, continuing...\n".green)
+}
+
 keypress(process.stdin);
 
 process.stdin.on('keypress', function(ch, key){
@@ -10,12 +33,28 @@ process.stdin.on('keypress', function(ch, key){
   if(key.name === 'escape'){
     console.log('Escape pressed, exiting...');
     process.exit();
-  }else{
-    if(isKeyDown === ch) {
-      console.log('Key: ' + ch + ' is held...');
-      isKeyDown = ch;
-    }
+  }else if (ch==='w') {
+    //both motors forward
+    leftMotor.runForever(75);
+    rightMotor.runForever(75);
+  }else if (ch==='a') {
+    leftMotor.runForever(-75);
+    rightMotor.runForever(75);
+  }else if (ch==='d') {
+    leftMotor.runForever(75);
+    rightMotor.runForever(-75);
+  }else if (ch==='s') {
+    leftMotor.runForever(-75);
+    rightMotor.runForever(-75);
+  }else if (ch==='w' && ch==='a') {
+    leftMotor.runForever(30);
+    rightMotor.runForever(85);
+  }else if (ch==='w' && ch==='d') {
+    leftMotor.runForever(85);
+    rightMotor.runForever(30);
   }
+  leftMotor.stop();
+  rightMotor.stop();
 });
 
 process.stdin.setRawMode(true);
