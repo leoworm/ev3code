2a57a94b97e285523847a086c9133e1fdd930834
leoworm <leo.charles.p@gmail.com>
2017-04-26 22:38:40 +0200

    Fixed potential bug
    This bug could have stopped the robot every half second or so.

--- controller.js
+++ controller.js
@@ -1,11 +1,14 @@
-//TODO switch to http and sockets so electron can be used to make a GUI
+//TODO switch to http and sockets. host a website with login for controlling the robot.
 //All modules are required here. They must be installed with the --save argument
 var colors = require('colors');
 var keypress = require('keypress');
 var ev3 = require('ev3dev-lang');
 
-var leftMotor = new ev3.LargeMotor('port')
-var rightMotor = new ev3.LargeMotor('port')
+var leftMotorPort = 'A';
+var rightMotorPort = 'B';
+
+var leftMotor = new ev3.LargeMotor(leftMotorPort);
+var rightMotor = new ev3.LargeMotor(rightMotorPort);
 
 var leftConnected = true;
 var rightConnected = true;
@@ -23,7 +26,7 @@ if(rightConnected==false || leftConnected==false) {
   console.log('Something is wrong, aborting...'.red);
   process.exit();
 }else{
-  console.log("Motors seem connected, continuing...\n".green)
+  console.log("Motors seem connected, continuing...\n".green);
 }
 
 keypress(process.stdin);
@@ -52,9 +55,10 @@ process.stdin.on('keypress', function(ch, key){
   }else if (ch==='w' && ch==='d') {
     leftMotor.runForever(85);
     rightMotor.runForever(30);
+  }else{
+    leftMotor.stop();
+    rightMotor.stop();
   }
-  leftMotor.stop();
-  rightMotor.stop();
 });
 
 process.stdin.setRawMode(true);
